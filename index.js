24e6fc9a0fae8660db18b55255669d990da1c78f
Moumen Hallak <moumen95@gmail.com>
2021-06-20 21:08:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 // Code your solution here
 function findMatching(drivers, string){
   return drivers.filter(n => {
-    return (n.toLowerCase() === string.toLowerCase)
+    return (n.toLowerCase() === string.toLowerCase())
   })
 }
 
-function findMatching(drivers, name){
-  return drivers.filter( find_name => {
-    return (find_name.toLowerCase() === name.toLowerCase())
-   }
-  )
-}
