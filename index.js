bee50aff9b6dd6545b5d6f7f92e26682d9156414
Moumen Hallak <moumen95@gmail.com>
2021-06-20 21:18:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(drivers, string){
+  return drivers.filter(n => {
+    return (n.toLowerCase() === string.toLowerCase());
+  });
+}
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(n => {
+    if (string.charAt(0) === n.charAt(0)){
+    return n;
+    }
+  });
+}
+
+function matchName(drivers, name){
+  return drivers.filter(n => {
+    if (n.name == name){
+      return n
+    }
+  })
+}
+
+
